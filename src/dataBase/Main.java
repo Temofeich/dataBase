c243883a4bc89dd10fa12a8e4b19144772af1604
Temofeich <artem_antonovich_1988@mail.ru>
2020-09-22 19:02:23 +0300

    connect java with MySQL

--- src/dataBase/Main.java
+++ src/dataBase/Main.java
@@ -8,6 +8,7 @@ public class Main {
     String connectionURL = "jdbc:mysql://localhost:3306/mysql";
     Class.forName("com.mysql.cj.jdbc.Driver");
 
+    
     try(Connection connection = DriverManager.getConnection(connectionURL,userName,password)){
         System.out.println("соединение прошло успешно!");
     }
