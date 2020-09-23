e301c149a255d320516804d8d3c5785876736165
Temofeich <artem_antonovich_1988@mail.ru>
2020-09-23 19:34:49 +0300

    connect java with MySQL

--- src/dataBase/Main.java
+++ src/dataBase/Main.java
@@ -1,5 +1,6 @@
 package dataBase;
 import java.sql.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException{
@@ -8,7 +9,6 @@ public class Main {
     String connectionURL = "jdbc:mysql://localhost:3306/mysql";
     Class.forName("com.mysql.cj.jdbc.Driver");
 
-    
     try(Connection connection = DriverManager.getConnection(connectionURL,userName,password)){
         System.out.println("соединение прошло успешно!");
     }
